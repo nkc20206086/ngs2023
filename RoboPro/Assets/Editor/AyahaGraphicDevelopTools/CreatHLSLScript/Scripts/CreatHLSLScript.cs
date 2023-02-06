5cb9f6ffc00e6235c98271a25a5330cb47e4117d
ayaha401 <oshizawadai.1192@gmail.com>
2023-02-06 18:36:21 +0900

    HLSLを作れるアセット追加

---
+++ RoboPro/Assets/Editor/AyahaGraphicDevelopTools/CreatHLSLScript/Scripts/CreatHLSLScript.cs
@@ -0,0 +1,28 @@
+﻿//====================================================================================================
+// v1.1.0
+// Twitter : @ayaha__401
+//====================================================================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class CreatHLSLScript : EditorWindow
+{
+    static string path;
+
+    [MenuItem("Assets/Create/Shader/HLSLScript")]
+    private static void CreatHLSL()
+    {
+        path = AssetDatabase.GetAssetPath(Selection.activeInstanceID);
+        path = EditorUtility.SaveFilePanelInProject("CreatHLSLScript", "NewShader", "hlsl", "", path);
+        if (!string.IsNullOrEmpty(path))
+        {
+            path = AssetDatabase.GenerateUniqueAssetPath(path);
+            
+            File.WriteAllText(path,"");
+            AssetDatabase.Refresh();
+        }
+    }
+}
