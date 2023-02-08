1095af1e9b68d5f505e7b70b9b27991f9da39f32
kmamonnassi <ka82573@jk2.so-net.ne.jp>
2023-02-08 12:41:47 +0900

    コメントアウトを追加

--- RoboPro/Assets/Scripts/StageSelect/Model/StageSelectModel.cs
+++ RoboPro/Assets/Scripts/StageSelect/Model/StageSelectModel.cs
@@ -12,12 +12,14 @@ namespace Robo
         private int stageIndex;
         private int length;
 
+        //初期化
         public void Initalize(StageSelectModelArgs args)
         {
             length = args.StageLength;
             OnInitalize?.Invoke(args);
         }
 
+        //ステージを選択s
         public void Select(int index)
         {
             if (index >= length || index < 0)
@@ -30,16 +32,19 @@ namespace Robo
             OnSelect?.Invoke(stageIndex);
         }
 
+        //次のステージを選択
         public void SelectNext()
         {
             Select(stageIndex + 1);
         }
 
+        //前のステージを選択
         public void SelectPrevious()
         {
             Select(stageIndex - 1);
         }
 
+        //ステージをプレイ
         public void Play()
         {
             UnityEngine.Debug.Log("ステージ"+ stageIndex + "をプレイ");
--- RoboPro/Assets/Scripts/StageSelect/View/StageSelectView.cs
+++ RoboPro/Assets/Scripts/StageSelect/View/StageSelectView.cs
@@ -27,12 +27,14 @@ namespace Robo
             }
         }
 
+        //ステージ選択
         public void Select(int idx)
         {
             Vector3 position = elements[idx].transform.localPosition;
             elementsParent.DOLocalMoveX(-position.x, moveDuration);
         }
 
+        //選択不可能範囲に移動すると警告が出る
         public void SelectError(int idx)
         {
             Debug.Log("これ以上進めない");
@@ -40,15 +42,18 @@ namespace Robo
 
         private void Update()
         {
+            //Dキーで右に移動
             if(Input.GetKeyDown(KeyCode.D))
             {
                 OnSelectNextKey?.Invoke();
             }
             else
+            //Aキーで左に移動
             if (Input.GetKeyDown(KeyCode.A))
             {
                 OnSelectPreviousKey?.Invoke();
             }
+            //Spaceでステージをプレイ
             if(Input.GetKeyDown(KeyCode.Space))
             {
                 OnPlay?.Invoke();
