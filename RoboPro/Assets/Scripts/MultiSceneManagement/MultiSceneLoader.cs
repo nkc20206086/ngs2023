d547e5e0262b4dfba3b18fa9fef6299f4f781ba8
kmamonnassi <ka82573@jk2.so-net.ne.jp>
2023-02-22 12:06:28 +0900

    インターフェースを明示的に実装

--- RoboPro/Assets/Scripts/MultiSceneManagement/MultiSceneLoader.cs
+++ RoboPro/Assets/Scripts/MultiSceneManagement/MultiSceneLoader.cs
@@ -23,21 +23,23 @@ namespace Robo
 
         //シーン追加
         //extraBindingsで、追加するシーンに今のシーンの情報をバインドできる。
-        public async UniTask AddScene(SceneID id, bool autoChangeActiveScene,  Action<DiContainer> extraBindings)
+        async UniTask IMultiSceneLoader.AddScene(SceneID id, bool autoChangeActiveScene,  Action<DiContainer> extraBindings)
         {
             await zenjectSceneLoader.LoadSceneAsync(id.ToString(), LoadSceneMode.Additive, extraBindings);
             if (autoChangeActiveScene)
             {
-                SetActiveScene(id);
+                ((IMultiSceneLoader)this).SetActiveScene(id);
             }
             OnAddScene?.Invoke(id);
         }
 
         //シーン追加
-        public async UniTask AddScene(SceneID id, bool autoChangeActiveScene) => await AddScene(id, autoChangeActiveScene, null);
-
+        async UniTask IMultiSceneLoader.AddScene(SceneID id, bool autoChangeActiveScene)
+        {
+            await ((IMultiSceneLoader)this).AddScene(id, autoChangeActiveScene, null);
+        }
         //シーン削除
-        public async UniTask UnloadScene(SceneID id)
+        async UniTask IMultiSceneLoader.UnloadScene(SceneID id)
         {
             await SceneManager.UnloadSceneAsync(id.ToString());
             await Resources.UnloadUnusedAssets();
@@ -45,7 +47,7 @@ namespace Robo
         }
 
         //アクティブシーンを決定する
-        public void SetActiveScene(SceneID id)
+        void IMultiSceneLoader.SetActiveScene(SceneID id)
         {
             SceneManager.SetActiveScene(SceneManager.GetSceneByName(id.ToString()));
             OnActiveScene?.Invoke(new SetActiveSceneArgs(nowActiveScene, id));
