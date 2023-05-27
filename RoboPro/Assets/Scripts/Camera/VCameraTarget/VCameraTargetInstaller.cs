1b74fd1efcb43da9d51cf9fc43aabcd6fced9ef6
nkc-TakaoOnda <tnk5zdmr7uegh4i2i13m@icloud.com>
2023-05-27 10:17:00 +0900

    pro:edit Camera_Zenject 書いたはずのZenjectが消えてて、CameraとかPlayerがエラーしてたので直してプッシュします。

--- RoboPro/Assets/Scripts/Camera/VCameraTarget/VCameraTargetInstaller.cs
+++ RoboPro/Assets/Scripts/Camera/VCameraTarget/VCameraTargetInstaller.cs
@@ -4,9 +4,11 @@ using Zenject;
 public class VCameraTargetInstaller : MonoInstaller
 {
     [SerializeField] private VCameraTargetChanger vCameraTargetChanger;
+    [SerializeField] private MainCamera.CameraCalcForword calcForword;
 
     public override void InstallBindings()
     {
         Container.Bind<IVCameraTargetChanger>().FromInstance(vCameraTargetChanger);
+        Container.Bind<MainCamera.ICameraVectorGetter>().FromInstance(calcForword);
     }
 }
