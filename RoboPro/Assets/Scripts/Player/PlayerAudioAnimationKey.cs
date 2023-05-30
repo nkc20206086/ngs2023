9fb865a07c72ab14322ef0bb821a8f3881872409
nkc-TakaoOnda <tnk5zdmr7uegh4i2i13m@icloud.com>
2023-05-31 00:53:35 +0900

    pro:edit Player_SE playerにSEを追加しました。

---
+++ RoboPro/Assets/Scripts/Player/PlayerAudioAnimationKey.cs
@@ -0,0 +1,46 @@
+using Robo;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class PlayerAudioAnimationKey : MonoBehaviour
+{
+    [Inject]
+    private IAudioPlayer audioPlayer;
+
+    public void Move_SE()
+    {
+        audioPlayer.PlaySE(CueSheetType.Player, "SE_Player_Walk");
+    }
+
+    public void Jump_SE()
+    {
+        audioPlayer.PlaySE(CueSheetType.Player, "SE_Player_Jump");
+    }
+
+    public void Landing_SE()
+    {
+        audioPlayer.PlaySE(CueSheetType.Player, "SE_Player_Landing");
+    }
+
+    public void Access_SE()
+    {
+        audioPlayer.PlaySE(CueSheetType.Player, "SE_Player_Access");
+    }
+
+    public void Goal_Access_SE()
+    {
+        audioPlayer.PlaySE(CueSheetType.Player, "SE_Player_GoalAccess");
+    }
+
+    public void Death_SE()
+    {
+        audioPlayer.PlaySE(CueSheetType.Player, "SE_Player_Death");
+    }
+
+    public void Goal_Staging_SE()
+    {
+        audioPlayer.PlaySE(CueSheetType.Staging, "SE_Staging_Goal");
+    }
+}
