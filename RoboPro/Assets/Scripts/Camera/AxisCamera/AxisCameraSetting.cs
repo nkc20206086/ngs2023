b7a68836adcfb18498aeff5516fc00f4c0f741c5
ayaha401 <oshizawadai.1192@gmail.com>
2023-05-31 01:29:11 +0900

    pro:add 軸を表示するUIを作成

---
+++ RoboPro/Assets/Scripts/Camera/AxisCamera/AxisCameraSetting.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace AxisCamera
+{
+    public class AxisCameraSetting : MonoBehaviour, IAxisCameraSettable
+    {
+        [SerializeField]
+        private GameObject axisObj;
+
+        private Camera mainCamera;
+        private Camera axisCamera;
+        private ReactiveProperty<Quaternion> mainCameraRot = new ReactiveProperty<Quaternion>();
+
+        void Start()
+        {
+            mainCamera = transform.parent.GetComponent<Camera>();
+            axisCamera = GetComponent<Camera>();
+
+            mainCameraRot.Value = mainCamera.gameObject.transform.rotation;
+            mainCameraRot.Subscribe(r => axisObj.transform.localRotation = r)
+                         .AddTo(gameObject);
+        }
+
+        private void Update()
+        {
+            mainCameraRot.Value = mainCamera.gameObject.transform.rotation;
+        }
+
+        void IAxisCameraSettable.ShowAxisUI()
+        {
+            axisCamera.enabled = true;
+        }
+
+        void IAxisCameraSettable.HideAxisUI()
+        {
+            axisCamera.enabled = false;
+        }
+    }
+}
