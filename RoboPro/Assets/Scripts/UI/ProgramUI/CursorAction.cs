d4a1a0d2a83449cd1fd2956dca85417199ed3f31
Daiki_Shibata <39456536+kmamonnassi@users.noreply.github.com>
2023-05-31 18:37:37 +0900

    Update CursorAction.cs

--- RoboPro/Assets/Scripts/UI/ProgramUI/CursorAction.cs
+++ RoboPro/Assets/Scripts/UI/ProgramUI/CursorAction.cs
@@ -1,5 +1,12 @@
-using UnityEngine;
-
 public class CursorAction : MonoBehaviour
 {
+    [SerializeField]
+    private BehaviorButtonSelectView bbsv;
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            bbsv.SelectCrea();
+        }
+    }
 }
